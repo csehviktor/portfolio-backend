649864a81d66cfe91a81259871e0ddbe2e0f0198
csehviktor <csehviktor05@gmail.com>
2024-02-09 13:12:08 +0100

    handle exception by status check

--- src/main/kotlin/me/csehviktor/data/Track.kt
+++ src/main/kotlin/me/csehviktor/data/Track.kt
@@ -1,6 +1,8 @@
 package me.csehviktor.data
 
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import me.csehviktor.Constants.ACCESS_TOKEN
 import me.csehviktor.Constants.CLIENT
 import me.csehviktor.Constants.CURRENT_TRACK
@@ -13,15 +15,24 @@ class Track {
         val accessToken = ACCESS_TOKEN
 
         if(accessToken !== null) {
-            val response: CurrentTrackResponse = CLIENT.get("https://api.spotify.com/v1/me/player/currently-playing") {
+            val response: HttpResponse = CLIENT.get("https://api.spotify.com/v1/me/player/currently-playing") {
                 headers {
                     CurrentTrackRequest(
                         "Bearer $accessToken"
                     ).build(this)
                 }
-            }.parseTrack()
+            }
 
-            CURRENT_TRACK = CurrentTrackResponse(response.item, response.isPlaying)
+            CURRENT_TRACK = CurrentTrackResponse(null, false)
+
+            if(response.status != HttpStatusCode.NoContent) {
+                val parsedResponse = response.parseTrack()
+
+                CURRENT_TRACK = CurrentTrackResponse(
+                    parsedResponse.item,
+                    parsedResponse.isPlaying
+                )
+            }
 
             return CURRENT_TRACK
         }
